1fedd30df02c97d3d48589d9b7623eff9623b1cf
Laksh-D25 <laaaksh25@gmail.com>
2025-12-24 04:48:50 +0530

    Dumb Error

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useState } from 'react';
-import Mermaid from './Mermaid';
+import Mermaid from '@/components/Mermaid';
 
 export default function Home() {
   // Simple default flow
